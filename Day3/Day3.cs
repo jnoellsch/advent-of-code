55753cca1669d5414519f4b9099d76e11381d1ef
Jim Noellsch <jnoellsch@rightpoint.com>
2015-12-09 23:12:15 -0600

    Completed Day3, part 2

--- Day3/Day3.cs
+++ Day3/Day3.cs
@@ -8,30 +8,72 @@
     /// Plots the directions and delivery of presents to various houses. Determines the number of houses with 1 or more presents.
     /// http://adventofcode.com/day/3
     /// </summary>
-    public class Day3 : IPuzzle
+    public class Day3 : IPuzzle, IPuzzlePart2
     {
-        public object Answer()
+        object IPuzzle.Answer()
         {
             string elfDirections = File.ReadAllText("Day3/input.txt");
-            var giftTracker = new Dictionary<object, int>();
-            var plotter = new HousePlotter(); 
+            var giftTracker = new GiftTracker();
+            var plotter = new HousePlotter();
 
             foreach (char nextStop in elfDirections)
             {
                 plotter.Move(nextStop);
-
-                var house = plotter.CoordinateKey;
-                if (giftTracker.ContainsKey(house))
-                {
-                    giftTracker[house]++;
-                }
-                else
-                {
-                    giftTracker.Add(house, 1);
-                }
+                giftTracker.Record(plotter.CoordinateKey);
             }
 
             return giftTracker.LuckyHouses();
+        }
+
+        object IPuzzlePart2.Answer()
+        {
+            string elfDirections = File.ReadAllText("Day3/input.txt");
+            var giftTracker = new GiftTracker();
+            var santaPlotter = new HousePlotter();
+            var roboSantaPlotter = new HousePlotter();
+
+            // gift the starting house
+            giftTracker.Record(santaPlotter.CoordinateKey);
+
+            // follow directions for santa and robo santa
+            foreach (char nextStop in elfDirections.SantaOnly())
+            {
+                santaPlotter.Move(nextStop);
+                giftTracker.Record(santaPlotter.CoordinateKey);
+            }
+
+            foreach (char nextStop in elfDirections.RoboSantaOnly())
+            {
+                roboSantaPlotter.Move(nextStop);
+                giftTracker.Record(roboSantaPlotter.CoordinateKey);
+            }
+
+            return giftTracker.LuckyHouses();
+        }
+
+        public class GiftTracker
+        {
+            private IDictionary<object, int> _giftTracker = new Dictionary<object, int>();
+
+            public void Record(object house)
+            {
+                if (this._giftTracker.ContainsKey(house))
+                {
+                    this._giftTracker[house]++;
+                }
+                else
+                {
+                    this._giftTracker.Add(house, 1);
+                }
+            }
+
+            /// <summary>
+            /// Counts the number of houses with 1 or more presents.
+            /// </summary>
+            public int LuckyHouses()
+            {
+                return this._giftTracker.Where(x => x.Value >= 1).Count();
+            }
         }
 
         public class HousePlotter
@@ -78,11 +120,19 @@
     public static class Day3Extensions
     {
         /// <summary>
-        /// Counts the number of houses with 1 or more presents.
+        /// Extracts santa directions only (aka, odd slots).
         /// </summary>
-        public static int LuckyHouses(this IDictionary<object, int> input)
+        public static IEnumerable<char> SantaOnly(this string directions)
         {
-            return input.Where(x => x.Value >= 1).Count();
+            return directions.Where((x, i) => i % 2 != 0);
+        }
+
+        /// <summary>
+        /// Extracts robo santa directions only (aka, even slots).
+        /// </summary>
+        public static IEnumerable<char> RoboSantaOnly(this string directions)
+        {
+            return directions.Where((x, i) => i % 2 == 0);
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -8,9 +8,10 @@
         {
             ////IPuzzle part1 = new Day1();
             ////IPuzzlePart2 part2 = new Day1();
-            IPuzzle part1 = new Day2();
-            IPuzzlePart2 part2 = new Day2();
-            ////IPuzzle part1 = new Day3();
+            ////IPuzzle part1 = new Day2();
+            ////IPuzzlePart2 part2 = new Day2();
+            IPuzzle part1 = new Day3();
+            IPuzzlePart2 part2 = new Day3();
             ////IPuzzle part1 = new Day4();
 
             Console.WriteLine("Part 1: " + part1.Answer());
