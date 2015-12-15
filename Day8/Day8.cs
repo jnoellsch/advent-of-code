5dd4989652c3ee7c23c888041ad09701fe7fb4f3
Jim Noellsch <jnoellsch@rightpoint.com>
2015-12-14 23:14:07 -0600

    Day 8, part 1.

---
+++ Day8/Day8.cs
@@ -0,0 +1,73 @@
+﻿namespace AoC
+{
+    using System.IO;
+    using System.Text.RegularExpressions;
+
+    public class Day8 : IPuzzle, IPuzzlePart2
+    {
+        object IPuzzle.Answer()
+        {
+            string[] codeLines = File.ReadAllLines("Day8/input.txt");
+            var codeCounter = new CodeCounter();
+
+            foreach (var line in codeLines)
+            {
+                codeCounter.Count(line);
+            }
+
+            return codeCounter.CodeVsMemoryChars;
+        }
+
+        object IPuzzlePart2.Answer()
+        {
+            return string.Empty;
+        }
+
+        public class CodeCounter
+        {
+            public CodeCounter()
+            {
+                this.CodeChars = 0;
+                this.MemoryChars = 0;
+            }
+
+            public int CodeChars { get; private set; }
+
+            public int MemoryChars { get; private set; }
+
+            public int CodeVsMemoryChars
+            {
+                get
+                {
+                    return this.CodeChars - this.MemoryChars;
+                }
+            }
+
+            public void Count(string codeLine)
+            {
+                this.IncreaseCode(codeLine.Length);
+                this.IncreaseMemory(this.EncodeAsMemory(codeLine).Length);
+            }
+
+            private void IncreaseMemory(int amountToAdd)
+            {
+                this.MemoryChars += amountToAdd;
+            }
+
+            private void IncreaseCode(int amountToAdd)
+            {
+                this.CodeChars += amountToAdd;
+            }
+
+            private string EncodeAsMemory(string codeLine)
+            {
+                // normalize/kill code ends
+                codeLine = codeLine.TrimEnd('"').TrimStart('"'); 
+
+                // replace escape sequences
+                Regex regex = new Regex(@"\\x[a-f0-9][a-f0-9]|\\{2}|\\"""); 
+                return regex.Replace(codeLine, "_");
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -17,9 +17,11 @@
             ////IPuzzle part1 = new Day4();
             ////IPuzzlePart2 part2 = new Day4();
             ////IPuzzle part1 = new Day5();
-            IPuzzle part1 = new Day6();
-            IPuzzlePart2 part2 = new Day6();
-
+            ////IPuzzle part1 = new Day6();
+            ////IPuzzlePart2 part2 = new Day6();
+            IPuzzle part1 = new Day8();
+            IPuzzlePart2 part2 = new Day8();
+            
             Console.WriteLine("Part 1: " + part1.Answer());
             Console.WriteLine("Part 2: " + part2.Answer());
             Console.ReadKey();
