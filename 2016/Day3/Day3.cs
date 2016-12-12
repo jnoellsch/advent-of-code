a907d72233b5aec64de28ee5d319c45a8aa9f211
Jim Noellsch <jnoellsch@gmail.com>
2016-12-12 12:21:27 -0600

    Started Day3, Part1.

---
+++ 2016/Day3/Day3.cs
@@ -0,0 +1,42 @@
+﻿namespace AoC
+{
+    using System;
+    using System.IO;
+
+    /// <summary>
+    /// Validates the authenticity of triangle sizes on the wall.
+    /// http://adventofcode.com/2016/day/3
+    /// </summary>
+    public class Day3 : IPuzzle
+    {
+        public object Answer()
+        {
+            var checker = new TriangleChecker();
+            checker.GoAllOcdOnThatWall(File.ReadAllLines("Day3/input.txt"));
+            return checker.RealTrianglesCount;
+        }
+
+        public class TriangleChecker
+        {
+            public int RealTrianglesCount { get; private set; } = 0;
+
+            public void GoAllOcdOnThatWall(string[] triangleMeasurementLines)
+            {
+                foreach (var measurementLine in triangleMeasurementLines)
+                {
+                    var measurement = measurementLine.Split(new [] {' '}, StringSplitOptions.RemoveEmptyEntries);
+                    int side1 = Convert.ToInt32(measurement[0]);
+                    int side2 = Convert.ToInt32(measurement[1]);
+                    int side3 = Convert.ToInt32(measurement[2]);
+
+                    if (this.IsLegitTriangle(side1, side2, side3)) this.RealTrianglesCount++;
+                }
+            }
+
+            public bool IsLegitTriangle(int side1, int side2, int side3)
+            {
+                return (side1 + side2) > side3;
+            }
+        }
+    }
+}
--- 2016/Program.cs
+++ 2016/Program.cs
@@ -8,11 +8,13 @@
         {
             ////IPuzzle part1 = new Day1();
             ////IPuzzlePart2 part1 = new Day1();
-            IPuzzle part1 = new Day2();
-            IPuzzlePart2 part2 = new Day2();
+            ////IPuzzle part1 = new Day2();
+            ////IPuzzlePart2 part2 = new Day2();
+            IPuzzle part1 = new Day3();
+            ////IPuzzlePart2 part2 = new Day2();
 
             Console.WriteLine("Part 1: " + part1.Answer());
-            Console.WriteLine("Part 2: " + part2.Answer());
+            ////Console.WriteLine("Part 2: " + part2.Answer());
             Console.ReadKey();
         }
     }
