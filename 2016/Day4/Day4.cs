40a91667edc4a7ebf1769ae5d03d305bf59148ae
Jim Noellsch <jnoellsch@gmail.com>
2016-12-12 16:42:46 -0600

    Stubbed Day4, Part1.

---
+++ 2016/Day4/Day4.cs
@@ -0,0 +1,65 @@
+﻿namespace AoC
+{
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+
+    public class Day4 : IPuzzle
+    {
+        object IPuzzle.Answer()
+        {
+            var decryptor = new RoomListDecryptor();
+            decryptor.FindLegitRoomsForConductingBiznass(File.ReadAllLines("Day4/input.txt"));
+            return decryptor.SumRoomSectors();
+        }
+
+        public class RoomListDecryptor
+        {
+            public IList<Room> RealRooms { get; set; }
+
+            public void FindLegitRoomsForConductingBiznass(string[] roomLines)
+            {
+                foreach (string roomLine in roomLines)
+                {
+                    var r = Room.Parse(roomLine);
+                    if (r.IsValid())
+                    {
+                        this.RealRooms.Add(r);
+                    }
+                }
+            }
+
+            public int SumRoomSectors()
+            {
+                return this.RealRooms.Sum(x => x.Sector);
+            }
+        }
+
+        public class Room
+        {
+            private Room()
+            {
+            }
+
+            public int Sector { get; set; }
+
+            public string Checksum { get; set; }
+
+            public string Encrypted { get; set; }
+
+            public static Room Parse(string encryptedRoomInfo)
+            {
+                return new Room()
+                       {
+                           
+                       };
+            }
+
+            public bool IsValid()
+            {
+                throw new System.NotImplementedException();
+            }
+        }
+    }
+}
