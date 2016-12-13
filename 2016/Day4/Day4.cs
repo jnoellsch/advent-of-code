8e7cc6f37f18719eaf4702dd84d1992c69d1235d
Jim Noellsch <jnoellsch@gmail.com>
2016-12-13 14:43:33 -0600

    Finished Day4, Part1. LINQ + Regex, baby.

--- 2016/Day4/Day4.cs
+++ 2016/Day4/Day4.cs
@@ -1,9 +1,11 @@
 ﻿namespace AoC
 {
+    using System;
     using System.Collections;
     using System.Collections.Generic;
     using System.IO;
     using System.Linq;
+    using System.Text.RegularExpressions;
 
     public class Day4 : IPuzzle
     {
@@ -16,7 +18,7 @@
 
         public class RoomListDecryptor
         {
-            public IList<Room> RealRooms { get; set; }
+            public IList<Room> RealRooms { get; set; } = new List<Room>();
 
             public void FindLegitRoomsForConductingBiznass(string[] roomLines)
             {
@@ -42,23 +44,37 @@
             {
             }
 
-            public int Sector { get; set; }
+            public string Checksum { get; private set; }
 
-            public string Checksum { get; set; }
+            public string Cipher { get; private set; }
 
-            public string Encrypted { get; set; }
+            public int Sector { get; private set; }
 
             public static Room Parse(string encryptedRoomInfo)
             {
+                var regex = new Regex(@"(?<cipher>[a-z\-]*?)-(?<sector>\d*?)\[(?<checksum>.*?)\]");
+                var match = regex.Match(encryptedRoomInfo);
+
                 return new Room()
                        {
-                           
+                           Cipher = match.Groups["cipher"].Value.Replace("-", ""),
+                           Sector = Convert.ToInt32(match.Groups["sector"].Value),
+                           Checksum = match.Groups["checksum"].Value
                        };
             }
 
             public bool IsValid()
             {
-                throw new System.NotImplementedException();
+                var cipherOrderdCount =
+                    this.Cipher.ToCharArray()
+                        .GroupBy(x => x)
+                        .Select(grp => new { Character = grp.Key, Count = grp.Count() })
+                        .OrderByDescending(x => x.Count)
+                        .ThenBy(x => x.Character)
+                        .Take(5);
+
+                bool checksumMatches = cipherOrderdCount.Select(x => x.Character).Intersect(this.Checksum.ToCharArray()).Count() == 5;
+                return checksumMatches;
             }
         }
     }
