9979fd22fa0b8bb0e2632d9c9a5b292510bc2c73
Jim Noellsch <jnoellsch@gmail.com>
2016-12-26 16:14:33 -0600

    Added visual validation plus sample confirmation.

--- 2016/Day8/Day8.cs
+++ 2016/Day8/Day8.cs
@@ -1,15 +1,24 @@
 ﻿namespace AoC
 {
     using System;
+    using System.Collections.Generic;
     using System.IO;
+    using System.Linq;
     using System.Text.RegularExpressions;
 
     public class Day8 : IPuzzle
     {
+        private const int ON = 1;
+        private const int OFF = 0;
+        ////private const int ROW_SIZE = 50;
+        ////private const int COLUMN_SIZE = 6;
+        private const int ROW_DEPTH = 7;
+        private const int COLUMN_DEPTH = 3;
+
         object IPuzzle.Answer()
         {
             var screen = new ScreenFixer();
-            screen.ReadInstructionsFromCard(File.ReadAllLines("Day8/input.txt"));
+            screen.ReadInstructionsFromCard(File.ReadAllLines("Day8/sample.txt"));
             screen.SwipeCard();
 
             return screen.LitPixels;
@@ -19,7 +28,7 @@
         {
             public int LitPixels { get; set; }
 
-            public int[,] ScreenPixels { get; set; } = new int[50, 6];
+            public int[,] ScreenPixels { get; set; } = new int[COLUMN_DEPTH, ROW_DEPTH];
 
             public void ReadInstructionsFromCard(string[] instructions)
             {
@@ -29,12 +38,44 @@
                 {
                     var cmd = factory.Create(line);
                     cmd.Flip(this.ScreenPixels);
+
+                    this.DisplayLights();
                 }
             }
 
             public void SwipeCard()
             {
-                // cha-ching
+                // reset
+                this.LitPixels = 0;
+
+                // count  
+                for (int i = 0; i < COLUMN_DEPTH; i++)
+                {
+                    for (int j = 0; j < ROW_DEPTH; j++)
+                    {
+                        if(this.ScreenPixels[i, j] == ON)
+                        {
+                            this.LitPixels++;
+                        }
+                    }
+                }
+            }
+
+            private void DisplayLights()
+            {
+                for (int i = 0; i < COLUMN_DEPTH; i++)
+                {
+                    Console.Write("{0}: ", i);
+
+                    for (int j = 0; j < ROW_DEPTH; j++)
+                    {
+                        Console.Write("{0}", this.ScreenPixels[i, j] == ON ? "#" : ".");
+                    }
+
+                    Console.WriteLine();
+                }
+
+                Console.WriteLine();
             }
         }
 
@@ -91,6 +132,13 @@
 
             public void Flip(int[,] pixels)
             {
+                for (int i = 0; i < this.X; i++)
+                {
+                    for (int j = 0; j < this.Y; j++)
+                    {
+                        pixels[j, i] = ON;
+                    }
+                }
             }
         }
 
@@ -111,6 +159,60 @@
 
             public void Flip(int[,] pixels)
             {
+                if (this.Type == MoveType.Column)
+                {
+                    this.FlipColumn(pixels);
+                }
+                else if (this.Type == MoveType.Row)
+                {
+                    this.FlipRow(pixels);
+                }
+            }
+
+            private void FlipRow(int[,] pixels)
+            {
+                // convert row to linked list
+                var queue = new Queue<int>();
+                for (int i = ROW_DEPTH - 1; i >= 0; i--)
+                {
+                    queue.Enqueue(pixels[this.Index, i]);
+                }
+
+                // shift
+                this.ShiftQueue(queue);
+
+                // convert queue back to array (reverse read)
+                for (int i = ROW_DEPTH - 1; i >= 0; i--)
+                {
+                    pixels[this.Index, i] = queue.Dequeue();
+                }
+            }
+
+            private void FlipColumn(int[,] pixels)
+            {
+                // convert column to queue
+                var queue = new Queue<int>();
+                for (int i = COLUMN_DEPTH - 1; i >= 0; i--)
+                {
+                    queue.Enqueue(pixels[i, this.Index]);
+                }
+
+                // shift
+                this.ShiftQueue(queue);
+
+                // convert queue back to array (reverse read)
+                for (int i = COLUMN_DEPTH - 1; i >= 0; i--)
+                {
+                    pixels[i, this.Index] = queue.Dequeue();
+                }
+            }
+
+            private void ShiftQueue(Queue<int> queue)
+            {
+                for (int i = 0; i < this.Amount; i++)
+                {
+                    queue.Enqueue(queue.Dequeue());
+                }
             }
 
             public enum MoveType
