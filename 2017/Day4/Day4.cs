01b2a03bc8fb41f508792eae861287edb3ed1fa0
Jim Noellsch <jnoellsch@gmail.com>
2017-12-12 08:57:06 -0600

    2017 Day 4, Part 2.

--- 2017/Day4/Day4.cs
+++ 2017/Day4/Day4.cs
@@ -1,17 +1,26 @@
 ﻿namespace AoC
 {
+    using System;
     using System.Collections.Generic;
     using System.IO;
     using System.Linq;
     using AoC.Common;
 
-    public class Day4 : IPuzzle
+    public class Day4 : IPuzzle, IPuzzlePart2
     {
         public string[] Passphrases { get; } = File.ReadAllLines("Day4/input.txt");
 
         object IPuzzle.Answer()
         {
             var checker = new PassphraseChecker();
+            checker.Evaluate(this.Passphrases);
+
+            return checker.ValidCount;
+        }
+
+        object IPuzzlePart2.Answer()
+        {
+            var checker = new ComplexPassPhraseChecker();
             checker.Evaluate(this.Passphrases);
 
             return checker.ValidCount;
@@ -26,18 +35,32 @@
 
             public List<string> ValidPassphrases { get; } = new List<string>();
 
-            public void Evaluate(string[] passphrases)
+            protected virtual bool AllWordsAreDistinct(string[] words) => words.Distinct().Count() == words.Length;
+
+            public virtual void Evaluate(string[] passphrases)
             {
                 foreach (var phrase in passphrases)
                 {
                     var words = phrase.Split(' ');
-                    bool allWordsDistinct = words.Distinct().Count() == words.Length;
 
-                    if (allWordsDistinct)
+                    if (this.AllWordsAreDistinct(words))
                     {
                         this.ValidPassphrases.Add(phrase);
                     }
                 }
+            }
+        }
+
+        /// <summary>
+        /// Checks a bunch of passphrases for validity but words can be anagrams. A valid passphrases must contain no duplicate words 
+        /// or words with letters that can be rearranged to form other words. 
+        /// </summary>
+        public class ComplexPassPhraseChecker : PassphraseChecker
+        {
+            protected override bool AllWordsAreDistinct(string[] words)
+            {
+                var alphabeticalWords = words.Select(w => new string(w.ToCharArray().OrderBy(c => c).ToArray()));
+                return alphabeticalWords.Distinct().Count() == words.Length;
             }
         }
     }
--- 2017/Program.cs
+++ 2017/Program.cs
@@ -14,9 +14,10 @@
             ////IPuzzle part1 = new Day3();
             ////IPuzzlePart2 part2 = new Day3();
             IPuzzle part1 = new Day4();
+            IPuzzlePart2 part2 = new Day4();
 
             Console.WriteLine("Part 1: " + part1.Answer());
-            ////Console.WriteLine("Part 2: " + part2.Answer());
+            Console.WriteLine("Part 2: " + part2.Answer());
             Console.ReadKey();
         }
     }
