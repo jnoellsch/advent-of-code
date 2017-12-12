f6b4d07697ea7b276e758bcd6983c2350d45e421
Jim Noellsch <jnoellsch@gmail.com>
2017-12-12 08:43:49 -0600

    2017 Day 4, Part 1.

---
+++ 2017/Day4/Day4.cs
@@ -0,0 +1,44 @@
+﻿namespace AoC
+{
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using AoC.Common;
+
+    public class Day4 : IPuzzle
+    {
+        public string[] Passphrases { get; } = File.ReadAllLines("Day4/input.txt");
+
+        object IPuzzle.Answer()
+        {
+            var checker = new PassphraseChecker();
+            checker.Evaluate(this.Passphrases);
+
+            return checker.ValidCount;
+        }
+
+        /// <summary>
+        /// Checks a bunch of passphrases for validity. A valid passphrase must contain no duplicate words.
+        /// </summary>
+        public class PassphraseChecker
+        {
+            public int ValidCount => this.ValidPassphrases.Count;
+
+            public List<string> ValidPassphrases { get; } = new List<string>();
+
+            public void Evaluate(string[] passphrases)
+            {
+                foreach (var phrase in passphrases)
+                {
+                    var words = phrase.Split(' ');
+                    bool allWordsDistinct = words.Distinct().Count() == words.Length;
+
+                    if (allWordsDistinct)
+                    {
+                        this.ValidPassphrases.Add(phrase);
+                    }
+                }
+            }
+        }
+    }
+}
--- 2017/Program.cs
+++ 2017/Program.cs
@@ -13,8 +13,9 @@
             ////IPuzzlePart2 part2 = new Day2();
             ////IPuzzle part1 = new Day3();
             ////IPuzzlePart2 part2 = new Day3();
+            IPuzzle part1 = new Day4();
 
-            ////Console.WriteLine("Part 1: " + part1.Answer());
+            Console.WriteLine("Part 1: " + part1.Answer());
             ////Console.WriteLine("Part 2: " + part2.Answer());
             Console.ReadKey();
         }
