61cf60aa3b2913d4e216105b30d2cb4991201d3d
Jim Noellsch <jnoellsch@gmail.com>
2017-12-13 09:05:23 -0600

    2017 Day 5, Part 2.

--- 2017/Day5/Day5.cs
+++ 2017/Day5/Day5.cs
@@ -4,17 +4,27 @@
     using System.IO;
     using System.Linq;
     using AoC.Common;
-    public class Day5 : IPuzzle
-    {
-        public int[] JumpOffsets = File.ReadAllLines("Day5/input.txt").Select(c => Int32.Parse(c.ToString())).ToArray(); 
 
-        public object Answer()
+    public class Day5 : IPuzzle, IPuzzlePart2
+    {
+        public int[] JumpOffsets = File.ReadAllLines("Day5/input.txt").Select(c => Int32.Parse(c.ToString())).ToArray();
+
+        object IPuzzle.Answer()
         {
             var jumper = new InstructionJumper();
             jumper.FindExit(this.JumpOffsets);
 
             return jumper.StepsTaken;
         }
+
+        object IPuzzlePart2.Answer()
+        {
+            var jumper = new WeirdInstructionJumper();
+            jumper.FindExit(this.JumpOffsets);
+
+            return jumper.StepsTaken;
+        }
+       
 
         public class InstructionJumper
         {
@@ -33,12 +43,44 @@
                         break;
                     }
 
-                    // pop instruction. increment old instruction. move by instruction. track.
-                    var instruction = jumpOffsets[i];  
-                    jumpOffsets[i]++;                  
-                    i = i + instruction;               
+                    // track.
+                    i = this.CalculateOffset(jumpOffsets, i);
                     this.StepsTaken++;                 
                 }
+            }
+
+            /// <summary>
+            /// Pop instruction. Increment old instruction by 1. Move by instruction.
+            /// </summary>
+            protected virtual int CalculateOffset(int[] jumpOffsets, int i)
+            {
+                var instruction = jumpOffsets[i];
+                jumpOffsets[i]++;
+                i = i + instruction;
+                return i;
+            }
+        }
+
+        public class WeirdInstructionJumper : InstructionJumper
+        {
+            /// <summary>
+            /// Pop instruction. If the offset was three or more, instead decrease it by 1. Otherwise, increase it by 1. Move by instruction
+            /// </summary>
+            protected override int CalculateOffset(int[] jumpOffsets, int i)
+            {
+                var instruction = jumpOffsets[i];
+                if (instruction >= 3)
+                {
+                    jumpOffsets[i]--;
+
+                }
+                else
+                {
+                    jumpOffsets[i]++;
+                }
+
+                i = i + instruction;
+                return i;
             }
         }
     }
