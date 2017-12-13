ee3bb4ae63f3c95559e3989ad78a98cddc8bd03a
Jim Noellsch <jnoellsch@gmail.com>
2017-12-13 14:48:34 -0600

    Created extension method for incrementing and wrapping a number within a set. Fluid, baby.

---
+++ Common/IntExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace AoC.Common
+{
+    public static class IntExtensions
+    {
+        public static int Increment(this int number)
+        {
+            return number + 1;
+        }
+
+        public static int Wrap(this int index, int n)
+        {
+            return ((index % n) + n) % n;
+        }
+    }
+}
