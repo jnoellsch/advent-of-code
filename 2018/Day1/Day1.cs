d1af93690d453d56d44926afbbfa3538c1c2eb74
Jim Noellsch <jnoellsch@gmail.com>
2018-12-01 08:16:20 -0600

    2018 Day 1, Part 1.

---
+++ 2018/Day1/Day1.cs
@@ -0,0 +1,31 @@
+﻿namespace Aoc
+{
+    using System;
+    using System.Globalization;
+    using System.IO;
+    using System.Linq;
+    using AoC.Common;
+
+    public class Day1 : IPuzzle
+    {
+        public string[] Frequencies { get; set; } = File.ReadAllLines("Day1/input.txt");
+
+        public object Answer()
+        {
+            var calibrator = new DeviceCalibrator();
+            calibrator.Fix(this.Frequencies);
+
+            return calibrator.Frequency;
+        }
+    }
+
+    public class DeviceCalibrator
+    {
+        public int Frequency { get; set; } = 0;
+
+        public void Fix(string[] frequencies)
+        {
+            this.Frequency = frequencies.Select(f => Int32.Parse(f, NumberStyles.AllowLeadingSign)).Sum();
+        }
+    }
+}
---
+++ 2018/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Aoc
+{
+    using AoC.Common;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            IPuzzle part1 = new Day1();
+            ////IPuzzlePart2 part2 = new Day1();
+
+            Console.WriteLine("Part 1: " + part1.Answer());
+            ////Console.WriteLine("Part 2: " + part2.Answer());
+            Console.ReadKey();
+        }
+    }
+}
