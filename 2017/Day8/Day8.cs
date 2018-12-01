bd2c8b5c10cc777cef55beaed320c60713ba80f6
Jim Noellsch <jnoellsch@gmail.com>
2018-12-01 08:15:53 -0600

    2017 Day 8 stub

---
+++ 2017/Day8/Day8.cs
@@ -0,0 +1,31 @@
+﻿namespace AoC.Day8
+{
+    using System.IO;
+    using AoC.Common;
+
+    public class Day8 : IPuzzle
+    {
+        public string[] Instructions = File.ReadAllLines("Day8/input.txt");
+
+        object IPuzzle.Answer()
+        {
+            var manager = new RegisterManager();
+            manager.LoadAndRun(this.Instructions);
+
+            return manager.LargestValue;
+        }
+
+        public class RegisterManager
+        {
+            public void LoadAndRun(string[] instructions)
+            {
+                // convert instructions
+                // define unique registers
+                // run instructions
+                // find largest register value
+            }
+
+            public int LargestValue { get; set; }
+        }
+    }
+}
