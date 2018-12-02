16720a43945ece011ac223e79aeace10cd1fccf6
Jim Noellsch <jnoellsch@gmail.com>
2018-12-02 09:51:58 -0600

    2018 Day 2, Part 1.

---
+++ 2018/Day2/Day2.cs
@@ -0,0 +1,51 @@
+﻿namespace Aoc
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+    using AoC.Common;
+
+    public class Day2 : IPuzzle, IPuzzlePart2
+    {
+        public string[] BoxIds { get; set; } = File.ReadAllLines("Day2/input.txt");
+
+        object IPuzzle.Answer()
+        {
+            var tracker = new BoxTracker();
+            tracker.Calculate(this.BoxIds);
+
+            return tracker.Checksum;
+        }
+
+        object IPuzzlePart2.Answer()
+        {
+            return string.Empty;
+        }
+    }
+
+    public class BoxTracker
+    {
+        public int Checksum { get; private set; }
+
+        public void Calculate(string[] boxIds)
+        {
+            int twoCount = 0;
+            int threeCount = 0;
+
+            foreach (var id in boxIds)
+            {
+                // group by characters in box id
+                var groupedLetters = id
+                    .GroupBy(c => c)
+                    .Select(grp => new { Key = grp.Key, Count = grp.Count() })
+                    .ToList();
+
+                // count number of two and three groups. if multiple, just count it as one
+                twoCount += Math.Min(groupedLetters.Count(grp => grp.Count == 2), 1);
+                threeCount += Math.Min(groupedLetters.Count(grp => grp.Count == 3), 1);
+            }
+
+            this.Checksum = twoCount * threeCount;
+        }
+    }
+}
--- 2018/Program.cs
+++ 2018/Program.cs
@@ -8,8 +8,10 @@ namespace Aoc
     {
         public static void Main(string[] args)
         {
-            IPuzzle part1 = new Day1();
-            IPuzzlePart2 part2 = new Day1();
+            ////IPuzzle part1 = new Day1();
+            ////IPuzzlePart2 part2 = new Day1();
+            IPuzzle part1 = new Day2();
+            IPuzzlePart2 part2 = new Day2();
 
             Console.WriteLine("Part 1: " + part1.Answer());
             Console.WriteLine("Part 2: " + part2.Answer());
