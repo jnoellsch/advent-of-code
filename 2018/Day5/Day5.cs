0cd3d7c55d9ba06922dffdcb53305cf5104c11b2
Jim Noellsch <jnoellsch@gmail.com>
2018-12-05 10:58:33 -0600

    Day 5, Part 2.

--- 2018/Day5/Day5.cs
+++ 2018/Day5/Day5.cs
@@ -3,6 +3,7 @@
     using System;
     using System.Collections.Generic;
     using System.IO;
+    using System.Linq;
     using AoC.Common;
 
     public class Day5 : IPuzzle, IPuzzlePart2
@@ -17,7 +18,8 @@
 
         object IPuzzlePart2.Answer()
         {
-            return string.Empty;
+            var analyzer = new PolymerAnalyzer();
+            return analyzer.Shortest(this.Polymers);
         }
     }
 
@@ -25,25 +27,48 @@
     {
         protected List<string> ReactableUnits { get; } = new List<string>()
         {
-            "aA","bB","cC","dD","eE","fF","gG","hH","iI","jJ","kK","lL","mM","nN","oO","pP","qQ","rR","sS","tT","uU","vV","wW","xX","yY","zZ",
-            "Aa","Bb","Cc","Dd","Ee","Ff","Gg","Hh","Ii","Jj","Kk","Ll","Mm","Nn","Oo","Pp","Qq","Rr","Ss","Tt","Uu","Vv","Ww","Xx","Yy","Zz"
+            "Aa","Bb","Cc","Dd","Ee","Ff","Gg","Hh","Ii","Jj","Kk","Ll","Mm","Nn","Oo","Pp","Qq","Rr","Ss","Tt","Uu","Vv","Ww","Xx","Yy","Zz",
+            "aA","bB","cC","dD","eE","fF","gG","hH","iI","jJ","kK","lL","mM","nN","oO","pP","qQ","rR","sS","tT","uU","vV","wW","xX","yY","zZ"
         };
 
+        protected List<char> IndividualUnits { get; } = "abcdefghijklmnopqrstuvwxyz".ToList();
+
         public int Reduce(string polymer)
+        {
+            polymer = this.RunReducer(polymer);
+            return polymer.Length;
+        }
+
+        public int Shortest(string polymer)
+        {
+            var results = new List<string>();
+
+            foreach (var unit in this.IndividualUnits)
+            {
+                var polymerToModify = polymer;
+                polymerToModify = polymerToModify.Replace(unit.ToString(), string.Empty).Replace(unit.ToString().ToUpper(), string.Empty);
+
+                results.Add(this.RunReducer(polymerToModify));
+            }
+
+            return results.OrderBy(_ => _.Length).First().Length;
+        }
+
+        private string RunReducer(string polymer)
         {
             bool wasReduced;
             var prevPolymer = polymer;
 
             do
             {
-                this.ReactableUnits.ForEach(u => polymer = polymer.Replace(u,string.Empty));
+                this.ReactableUnits.ForEach(u => polymer = polymer.Replace(u, string.Empty));
 
                 wasReduced = prevPolymer.Length > polymer.Length;
                 prevPolymer = polymer;
             }
             while (wasReduced);
 
-            return polymer.Length;
+            return polymer;
         }
     }
 }
