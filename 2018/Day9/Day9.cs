2d5b36c55261992cb61c85c1134b2518a6719e9c
Jim Noellsch <jnoellsch@gmail.com>
2018-12-10 07:21:15 -0600

    Day 9 stub.

---
+++ 2018/Day9/Day9.cs
@@ -0,0 +1,20 @@
+﻿namespace Aoc
+{
+    using System.IO;
+    using AoC.Common;
+
+    public class Day9 : IPuzzle, IPuzzlePart2
+    {
+        private string Input { get; } = File.ReadAllText("Day9/sampleinput.txt");
+
+        object IPuzzle.Answer()
+        {
+            return string.Empty;
+        }
+
+        public object Answer()
+        {
+            return string.Empty;
+        }
+    }
+}
