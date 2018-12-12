ebf9dcb73a973d6b107223ca04b51cf74d104865
Jim Noellsch <jnoellsch@gmail.com>
2018-12-11 23:40:05 -0600

    Day 11, Part 2.

--- 2018/Day11/Day11.cs
+++ 2018/Day11/Day11.cs
@@ -18,13 +18,84 @@
 
         object IPuzzlePart2.Answer()
         {
-            return string.Empty;
+            var upp = new UltimatePowerPicker(9005);
+            upp.Calculate();
+
+            return upp.BestFuelCell;
+        }
+    }
+
+    internal class UltimatePowerPicker : PowerPicker
+    {
+        public UltimatePowerPicker(int serialNumber) : base(serialNumber)
+        {
+        }
+
+        private IList<CellPointWithSize> FuelSums { get; } = new List<CellPointWithSize>();
+
+        public override void Calculate()
+        {
+            this.Fill();
+            this.Sum();
+
+            var best = this.FuelSums.OrderByDescending(_ => _.Sum).First();
+            this.BestFuelCell = $"{best.Point.X},{best.Point.Y},{best.Size}";
+        }
+
+        private void Sum()
+        {
+            for (int y = 0; y < GridSize; y++)
+            {
+                for (int x = 0; x < GridSize; x++)
+                {
+                    int maxIndex = Math.Max(x, y);
+                    var point = new Point(x + 1, y + 1);
+
+                    for (int size = 0; size + maxIndex < GridSize; size++)
+                    {
+                        if (size == 0)
+                            continue;
+
+                        var cpws = new CellPointWithSize()
+                                   {
+                                       Point = point, 
+                                       Size = size + 1, 
+                                       Sum = this.PowerOfGrid(x, y, size + 1)
+                                   };
+
+                        if(cpws.Sum > 0)
+                            this.FuelSums.Add(cpws);
+                    }
+                }
+            }
+        }
+
+        private long PowerOfGrid(int startX, int startY, int size)
+        {
+            long sum = 0;
+
+            for (int y = startY; y < startY + size && y < GridSize; y++)
+            {
+                for (int x = startX; x < startX + size && x < GridSize; x++)
+                {
+                    sum += this.FuelGrid[x,y];
+                }
+            }
+
+            return sum;
+        }
+
+        private class CellPointWithSize
+        {
+            public Point Point { get; set; }
+            public int Size { get; set; }
+            public long Sum { get; set; }
         }
     }
 
     internal class PowerPicker
     {
-        private const int GridSize = 300;
+        protected const int GridSize = 300;
 
         public PowerPicker(int serialNumber)
         {
@@ -37,9 +108,9 @@
 
         private Dictionary<Point, int> FuelSums { get; } = new Dictionary<Point, int>(GridSize);
 
-        public string BestFuelCell { get; private set; }
+        public string BestFuelCell { get; protected set; }
 
-        public void Calculate()
+        public virtual void Calculate()
         {
             this.Fill();
             this.Sum();
