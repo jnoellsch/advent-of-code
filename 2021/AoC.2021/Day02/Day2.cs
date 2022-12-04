d29883d638d9a8e58f01a824c41866a314728da6
Jim Noellsch <jnoellsch@gmail.com>
2022-12-04 09:41:16 -0600

    Day 2 2021

---
+++ 2021/AoC.2021/Day02/Day2.cs
@@ -0,0 +1,93 @@
+﻿namespace AoC
+{
+    using AoC.Common;
+    using System;
+    using System.Security.Claims;
+    using System.Text.RegularExpressions;
+
+    internal class Day2 : IPuzzle, IPuzzlePart2
+    {
+        private IEnumerable<SubmarineInstruction> Movements = File.ReadAllLines("Day02/input.txt").Select(SubmarineInstruction.Parse);
+
+        object IPuzzle.Answer()
+        {
+            var sub = new Submarine();
+            sub.Move(this.Movements);
+            return sub.Position;
+        }
+
+        object IPuzzlePart2.Answer()
+        {
+            var sub = new ShootingSubmarine();
+            sub.Move(this.Movements);
+            return sub.Position;
+        }
+    }
+
+    internal class ShootingSubmarine : Submarine
+    {
+        public int Aim { get; private set; }
+
+        public override void Move(IEnumerable<SubmarineInstruction> movements)
+        {
+            foreach (var m in movements)
+            {
+                if (m.IsHorizontalMove)
+                {
+                    this.Xpos += m.Amount;
+                    this.Ypos += m.Amount * this.Aim;
+                }
+                else
+                {   
+                    this.Aim += m.Amount;
+                }
+            }
+        }
+    }
+
+    internal class Submarine
+    {
+        public int Xpos { get; protected set; } = 0;
+        public int Ypos { get; protected set; } = 0;
+        public int Position => this.Xpos * this.Ypos;
+
+        public virtual void Move(IEnumerable<SubmarineInstruction> movements)
+        {
+            foreach (var m in movements)
+            {
+                if(m.IsHorizontalMove)
+                {
+                    this.Xpos += m.Amount;
+                }
+                else
+                {
+                    this.Ypos += m.Amount;
+                }
+            }
+        }
+    }
+
+    internal class SubmarineInstruction
+    {
+        public string Movement { get; private set; } = string.Empty;
+        public int Amount { get; private set; } = -1;
+        public bool IsHorizontalMove => this.Movement.Equals("forward", StringComparison.OrdinalIgnoreCase);
+
+        internal static SubmarineInstruction Parse(string input)
+        {
+            Regex regex = new Regex(@"(?<mv>\w+) (?<amt>\d+)");
+            Match match = regex.Match(input);
+
+            int amt = Convert.ToInt32(match.Groups["amt"].Value);
+            string mv = match.Groups["mv"].Value;
+
+            return new SubmarineInstruction()
+            {
+                Movement = mv,
+                Amount = IsNegativeMove(mv) ? -amt : amt
+            };
+        }
+
+        private static bool IsNegativeMove(string mv) => mv.Equals("up", StringComparison.OrdinalIgnoreCase);
+    }
+}
--- 2021/AoC.2021/Program.cs
+++ 2021/AoC.2021/Program.cs
@@ -1,8 +1,10 @@
 ﻿using AoC;
 using AoC.Common;
 
-IPuzzle part1 = new Day1();
-IPuzzlePart2 part2 = new Day1();
+////IPuzzle part1 = new Day1();
+////IPuzzlePart2 part2 = new Day1();
+IPuzzle part1 = new Day2();
+IPuzzlePart2 part2 = new Day2();
 
 Console.WriteLine("Part 1: " + part1.Answer());
 Console.WriteLine("Part 2: " + part2.Answer());
