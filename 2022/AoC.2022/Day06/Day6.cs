ee1e601df2690be8b4b6587505aced6717d318a2
Jim Noellsch <jnoellsch@rightpoint.com>
2022-12-06 14:09:45 -0600

    Day 6 2022

---
+++ 2022/AoC.2022/Day06/Day6.cs
@@ -0,0 +1,45 @@
+﻿namespace AoC
+{
+    using AoC.Common;
+    using System;
+    using System.Diagnostics;
+    using static AoC.Day5;
+
+    internal class Day6 : IPuzzle, IPuzzlePart2
+    {
+        private string Datastream = File.ReadAllText("Day06/input.txt");
+
+        object IPuzzle.Answer()
+        {
+            var system = new CommunicationsSystem();
+            Debug.WriteLine("{0} = 7", system.FindMarker("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 4));
+            Debug.WriteLine("{0} = 5", system.FindMarker("bvwbjplbgvbhsrlpgdmjqwftvncz", 4));
+            Debug.WriteLine("{0} = 6", system.FindMarker("nppdvjthqldpwncqszvftbrmjlhg", 4));
+            Debug.WriteLine("{0} = 10", system.FindMarker("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 4));
+            Debug.WriteLine("{0} = 11", system.FindMarker("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 4));
+            return system.FindMarker(this.Datastream, 4);
+        }
+
+        object IPuzzlePart2.Answer()
+        {
+            var system = new CommunicationsSystem();
+            return system.FindMarker(this.Datastream, 14);
+        }
+    }
+
+    internal class CommunicationsSystem
+    {
+        internal int FindMarker(string datastream, int length)
+        {
+            for (int i = 0; i < datastream.Length - length; i++)
+            {
+                if(datastream.Skip(i).Take(length).Distinct().Count() == length)
+                {
+                    return i + length;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
--- 2022/AoC.2022/Program.cs
+++ 2022/AoC.2022/Program.cs
@@ -9,8 +9,10 @@ using AoC.Common;
 ////IPuzzlePart2 part2 = new Day3();
 ////IPuzzle part1 = new Day4();
 ////IPuzzlePart2 part2 = new Day4();
-IPuzzle part1 = new Day5();
-IPuzzlePart2 part2 = new Day5();
+////IPuzzle part1 = new Day5();
+////IPuzzlePart2 part2 = new Day5();
+IPuzzle part1 = new Day6();
+IPuzzlePart2 part2 = new Day6();
 
 Console.WriteLine("2022 Part 1: " + part1.Answer());
 Console.WriteLine("2022 Part 2: " + part2.Answer());
